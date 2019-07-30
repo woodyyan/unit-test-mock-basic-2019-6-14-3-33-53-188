3b7fc50e9716fc1621ec694e057cee164a280e80
Songbai Yan <colorguitar@hotmail.com>
2019-07-30 15:29:09 +0800

    finish

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -2,8 +2,9 @@ package cashregister;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 public class CashRegisterTest {
 
@@ -11,15 +12,33 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+        Printer printer = Mockito.mock(Printer.class);
+        printer.print("A\t123\n");
+        CashRegister cashRegister = new CashRegister(printer);
+        Item[] items = new Item[]{new Item("A", 123)};
+        Purchase purchase = new Purchase(items);
+
         //when
+        cashRegister.process(purchase);
+
         //then
+        Mockito.verify(printer, times(1)).print("A\t123\n");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
         //given
+        Printer printer = Mockito.mock(Printer.class);
+        printer.print("A\t123\n");
+        CashRegister cashRegister = new CashRegister(printer);
+        Item[] items = new Item[]{new Item("A", 123)};
+        Purchase purchase = Mockito.mock(Purchase.class);
+
         //when
+        cashRegister.process(purchase);
+
         //then
+        Mockito.verify(printer, times(1)).print("A\t123\n");
     }
 
     @Test
@@ -28,5 +47,4 @@ public class CashRegisterTest {
         //when
         //then
     }
-
 }
